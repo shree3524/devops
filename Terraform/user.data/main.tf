e34c614fc8f362241350a650383c041ed1cfe0be
shree3524 <117713811+shree3524@users.noreply.github.com>
2024-06-19 15:48:28 +0530

    Update main.tf

--- Terraform/user.data/main.tf
+++ Terraform/user.data/main.tf
@@ -1,1 +1,15 @@
-
+resource "aws_instance" "shree" {
+  ami = "ami-0e326862c8e74c0fe"
+  key_name = "pin"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "shree"
+  }
+    user_data = <<-EOF
+    #!/bin/bash
+    sudo yum install nginx -y 
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+    sudo echo "hello shree" > /usr/share/nginx/html/index.html 
+EOF
+}
