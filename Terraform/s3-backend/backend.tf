7815538967ca8947ccdbe07011d2d35a8d052d29
shree3524 <117713811+shree3524@users.noreply.github.com>
2024-06-26 15:18:03 +0530

    Update backend.tf

--- Terraform/s3-backend/backend.tf
+++ Terraform/s3-backend/backend.tf
@@ -1,1 +1,20 @@
+terraform {
+  backend "s3" {
+    profile = "shree"
+    bucket = "my-kitkat"
+    key = "terraform.tfstate"
+    region = "ap-southeast-2"
 
+    
+  }
+}
+
+resource "aws_instance" "bu" {
+    ami = "ami-0cb3193ac3a1520ce"
+    instance_type = "t3.micro"
+     
+    tags = {
+      Name = "pri"
+    }
+
+}
